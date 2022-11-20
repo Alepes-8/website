8c05c27cc0dcc631b600ec2348b5d1f9dcbc45b1
oliolo <oliver_olofsson@hotmail.se>
2022-11-20 14:41:28 +0100

    switching between pages now works

---
+++ src/pages/recipePage/RecipePage.jsx
@@ -0,0 +1,16 @@
+import './recipePage.css';
+import React from "react";
+
+const RecipePage = () => {
+    return (
+      <div className="RecipePage">
+        
+        <h1>
+            You have reached the recipe page.
+        </h1>
+
+      </div>
+    );
+  }
+  
+  export default RecipePage;
---
+++ src/pages/startPage/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.css';
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Home = () => {
+  return (
+    <div className="SearchPage">
+      
+      <h1>
+          You have reached the Home page.
+      </h1>
+
+    </div>
+  );
+};
+
+export default Home;
