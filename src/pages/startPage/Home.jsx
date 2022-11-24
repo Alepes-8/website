f929cc2ca278af3cd644d2c9d03b6b1d151968da
Alex P <Alex.peschel@gmail.com>
2022-11-24 14:18:06 +0100

    catagories implemented into develop

--- src/pages/startPage/Home.jsx
+++ src/pages/startPage/Home.jsx
@@ -1,18 +1,20 @@
 import './Home.css';
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
-import {Categories} from '../../containers';
+import {Categories,FeaturedFrontPage} from '../../containers';
 
 const Home = () => {
   return (
     <div className="SearchPage">
       
       <h1>
+      <FeaturedFrontPage />
       <div className='categories'>
           <Categories/>
         </div>
       </h1>
 
+      
     </div>
   );
 };
