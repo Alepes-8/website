8a9e0983ccbf902b29aa02031d9afa2bf4b379be
Alex P <Alex.peschel@gmail.com>
2022-11-28 21:49:23 +0100

    add base layout for the recipe page

--- src/containers/search/Search.jsx
+++ src/containers/search/Search.jsx
@@ -4,8 +4,14 @@ import './search.css';
 const search = () => {
   return (
     <div className='search_page'>
-      <div className='search_page_search'><p>Where the search part should be</p></div>
-      <div className='search_page_result'><p>the results of the search part</p></div>
+      <div className='search_page_search'>
+        <div className='search_text_box '></div>
+        <div className='search_catagories'></div>
+      </div>
+      <div className='search_page_result'>
+        <div className='results_sorting'></div>
+        <div className='results_seaction'></div>
+      </div>
     </div>
   )
 }
--- src/pages/recipePage/RecipePage.jsx
+++ src/pages/recipePage/RecipePage.jsx
@@ -1,14 +1,53 @@
 import './recipePage.css';
 import React from "react";
+import temp from '../../assets/temp.png';
 
 const RecipePage = () => {
     return (
       <div className="RecipePage">
-        
-        <h1>
-            You have reached the recipe page.
-        </h1>
+        <div className='Recipe_Title'>
+          <h1>Temp recipe title</h1>
+        </div>
+        <div className='Recipe_Image'>
+        <img src={temp} alt="logo"/>
+        </div>
+        <div className='Recipe_Content_Holder'>
 
+          <div className='Recipe_Content'>
+            <div className='Recipe_Content_Igredients'>
+              <div className='Recipe_Content_Ingredients_Title'>
+                <p> Ingredience list </p>
+              </div>
+              <div className='Recipe_Content_Ingredients_Serving'>
+                <p> Test ingredience serving</p>
+              </div>
+              <div className='Recipe_Content_Ingredients_List'>
+                <p> Test Ingredience list</p>
+              </div>
+            </div>
+            <div className='Recipe_Content_Direction'>
+              <p> Direction </p>
+            </div>
+          </div>
+        </div>
+        <div className='Recipe_Desciption'>
+          <div className='Recipe_Desciption_Title'>
+              <p>Decription</p>
+          </div>
+          <div className='Recipe_Desciption_MainText'>
+            <p>Main text decsriptbing the history of it</p>
+          </div>
+        </div>
+        <div className='Recipe_Comment_Area'>
+          <div className='Recipe_Coment'>
+            <div className='Recipe_Comment_Input'>
+              <p> Comment area</p>
+            </div>
+            <div className='Recipe_Comment_Submitment'>
+              <p> Future submit button</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
