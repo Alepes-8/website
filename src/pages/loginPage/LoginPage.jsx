789231ae280721c7e9fafa3c0b1c4edbce1295de
Alex P <Alex.peschel@gmail.com>
2022-12-01 18:21:07 +0100

    change the existing login with better layout

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,91 +1,80 @@
 import React, {useState} from 'react'
-
-import './loginPage.css';
-import { HiArrowNarrowRight } from 'react-icons/hi';
+import './authentication.css';
 
 
+const LoginPage = (props) => {
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
-
-function LoginPage() {
-  // React States
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-      password: "pass1"
-    },
-    {
-      username: "user2",
-      password: "pass2"
-    }
-  ];
-
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
-  };
-
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
-
-    var { uname, pass } = document.forms[0];
-
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
+    // User Login info, temp database
+    const database = [
+      {
+        username: "user1",
+        password: "pass1"
+      },
+      {
+        username: "user2",
+        password: "pass2"
       }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
-    }
-  };
+    ];
 
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
+
+    const errors = {
+      email: "invalid email",
+      pass: "invalid password"
+    };
+
+
+    /* here we submit the and handle the values which has been inputed.
+       In turn this will be where we probably contact the database*/
+       const handleSubmit = (event) => {
+        //Prevent page reload
+        event.preventDefault();
+    
+        var { email, pass } = document.forms[0];
+    
+        // Find user login info
+        const userData = database.find((user) => user.username === email.value);
+    
+        // Compare user info
+        if (userData) {
+          if (userData.password !== pass.value) {
+            // Invalid password
+            setErrorMessages({ name: "pass", message: errors.pass });
+          } else {
+            setIsSubmitted(true);
+          }
+        } else {
+          // Username not found
+          setErrorMessages({ name: "email", message: errors.email });
+        }
+      };
+
+
+    const renderErrorMessage = (name) =>
     name === errorMessages.name && (
       <div className="error">{errorMessages.message}</div>
     );
 
-  // JSX code for login form
-  const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Username </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
+    return (
+      <div className='login_page'>
+        <div className='auth_form_container'>
+          <div className='auth_form_style'>
+            <form className='login_form' onSubmit = {handleSubmit}>
+                  <label htmlFor="email">Email</label>
+                  <input  type="text" name="email"placeholder='Exemeple@gmail.com' required />
+                  {renderErrorMessage("email")}
+                  <label htmlFor="password">Password</label>
+                  <input type="password"  name="pass" placeholder='*********' required/>
+                  {renderErrorMessage("pass")}
+                  <button type="submit">Login </button>
+            </form>
+            <a href="/Register" classname ='link_button'>Don't have an account? Register</a>
+          </div>
         </div>
-        <div className="input-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
-        </div>
-        <div className="button-container">
-          <input type="submit" />
-        </div>
-      </form>
-    </div>
-  );
-
-  return (
-    <div className="login_page">
-      <div className="login-form">
-        <div className="title">Sign In</div>
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
       </div>
-    </div>
-  );
-}
+    );
+  }
+  
 
 export default LoginPage;
