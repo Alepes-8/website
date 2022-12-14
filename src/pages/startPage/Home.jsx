bc16d4f42cba3087c0e2711a369781b633a13072
oliolo <oliver_olofsson@hotmail.se>
2022-12-14 21:05:41 +0100

    connected to backend

--- src/pages/startPage/Home.jsx
+++ src/pages/startPage/Home.jsx
@@ -1,38 +1,40 @@
 import './Home.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import temp from '../../assets/testporkleg.png';
 import {Categories} from '../../containers';
+import { async } from 'q';
 
 
 const Home = () => {
+
+    let [recipes, setRecipes] = useState([])
+
+    useEffect(() => {
+        getRecipes()
+    }, [])
+
+    let getRecipes = async () => {
+        let response = await fetch("http://127.0.0.1:8000/recipes/")
+        let data = await response.json()
+        //console.log('DATA: ', data)
+        setRecipes(data)
+    }
+
   return (
     <div className="featured">
             <div className='recipe_alternatives'>
                
+                {recipes.map((recipe, index) => (
                     <a href='/RecipePage'>
                         <button className='recipe_alternative_button'>
-                            <p> Christian Pork Butt </p>
+                        <h3 key={index}> {recipe.name} </h3>
                             <img src={temp} alt="Logo" />
                         </button>
                     </a>
-                    <a href='/RecipePage'>
-                        <button className='recipe_alternative_button'>
-                            <p> Title </p>
-                            <img src={temp} alt="Logo" />
-                        </button>
-                    </a>
-                    <a href='/RecipePage'>
-                        <button className='recipe_alternative_button'>
-                            <p> Title </p>
-                            <img src={temp} alt="Logo" />
-                        </button>
-                    </a>
-                    <a href='/RecipePage'>
-                        <button className='recipe_alternative_button'>
-                            <p> Title </p>
-                            <img src={temp} alt="Logo" />
-                        </button>
-                    </a> 
+                ))}
+
+                    
+                    
                 
             </div>
             <div>
