ce12872c7dad6faca55ce67b18329e471005ff40
Alex P <Alex.peschel@gmail.com>
2022-12-19 01:00:54 +0100

    adding features to se the preview of the recipe

--- src/pages/recipePage/EditRecipe.jsx
+++ src/pages/recipePage/EditRecipe.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './editRecipe.css';
+import { useParams } from 'react-router-dom';
+import temp from '../../assets/testporkleg.png';
 import RecipePage from "./RecipePage";
 
 
-
 const EditRecipe = () => {
+    let {id} = useParams();
+    let recipeId = id;
+    let [recipes, setRecipe] = useState(null);
+
     const [action, setAction] = useState("");
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
@@ -13,84 +18,97 @@ const EditRecipe = () => {
     const [categories, setCategories] = useState("");
     const [ingredients, setIngredients] = useState("");
 
+    useEffect(() => {
+        getRecipe();
+    }, []);
+
+    const getRecipe = async() => {
+        let response = await fetch(`/recipes/${recipeId}/`);
+        let data = await response.json();
+        //console.log('DATA: ', data);
+        setRecipe(data);
+    }
 
   return (
     <div className='EditRecipePage'>
-    <div className="content">
-        <div className="UserSavedRecipes">
-            <h1>Edit Recipes</h1>
+        <div className="Editcontent">
+            <div className="UserSavedRecipes">
+                <h1>Edit Recipes</h1>
+                
+                <div className="form-group">
+                    <p>NewName</p>
+                    <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Enter Your Name"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Enter description"
+                    name="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input
+                    type="number"
+                    className="form-control form-control-lg"
+                    placeholder="Enter portionSize"
+                    name="portionSize"
+                    value={portionSize}
+                    onChange={(e) => setPortionSize(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                    type="date"
+                    className="form-control form-control-lg"
+                    placeholder="Enter creationDate"
+                    name="creationDate"
+                    value={creationDate}
+                    onChange={(e) => setCreationDate(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Enter categories"
+                    name="categories"
+                    value={categories}
+                    onChange={(e) => setCategories(e.target.value)}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Enter ingredients"
+                    name="ingredients"
+                    value={ingredients}
+                    onChange={(e) => setIngredients(e.target.value)}
+                    />
+                </div>
+
+                <button>Edit Recipe</button>
+                <p>{action}</p>
+            </div>
+            <div>
+                <RecipePage/>
             
-            <div className="form-group">
-                <p>NewName</p>
-                <input
-                type="text"
-                className="form-control form-control-lg"
-                placeholder="Enter Your Name"
-                name="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                />
             </div>
-
-            <div className="form-group">
-                <input
-                type="text"
-                className="form-control form-control-lg"
-                placeholder="Enter description"
-                name="description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                />
-            </div>
-
-            <div className="form-group">
-                <input
-                type="number"
-                className="form-control form-control-lg"
-                placeholder="Enter portionSize"
-                name="portionSize"
-                value={portionSize}
-                onChange={(e) => setPortionSize(e.target.value)}
-                />
-            </div>
-            <div className="form-group">
-                <input
-                type="date"
-                className="form-control form-control-lg"
-                placeholder="Enter creationDate"
-                name="creationDate"
-                value={creationDate}
-                onChange={(e) => setCreationDate(e.target.value)}
-                />
-            </div>
-            <div className="form-group">
-                <input
-                type="text"
-                className="form-control form-control-lg"
-                placeholder="Enter categories"
-                name="categories"
-                value={categories}
-                onChange={(e) => setCategories(e.target.value)}
-                />
-            </div>
-
-            <div className="form-group">
-                <input
-                type="text"
-                className="form-control form-control-lg"
-                placeholder="Enter ingredients"
-                name="ingredients"
-                value={ingredients}
-                onChange={(e) => setIngredients(e.target.value)}
-                />
-            </div>
-
-            <button>Edit Recipe</button>
-            <p>{action}</p>
         </div>
-       
         
-    </div>
     </div>
   )
 }
