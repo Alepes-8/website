0a25dd8137e62894214660a3b1a77a7b7f12e88f
Alex P <Alex.peschel@gmail.com>
2022-12-21 18:02:21 +0100

    toggle button, futher use for representing admin

--- src/components/admin/ManageUserUser.jsx
+++ src/components/admin/ManageUserUser.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import './manageUserUser.css';
+import { Toggle } from './Toggle'
+
 
 const ManageUsersUser = ({user}) => {
+   const ChangeAdminStatus = () => {
+
+   }
+    
     return( 
         <div className='User_Template'>
             
             <button> delete</button>
+            <div className='placement'>
+              
+            </div>
+           
             <b>ID:</b> {user.ID}
             <b>UserName:</b> {user.UserName} 
-                
+            <div className='toggle_Box'>
+                <input type="checkbox" defaultChecked={true} onClick={ChangeAdminStatus} />
+            </div>
+                <strong>{"Admin"}</strong>
+            
+           
             
         </div>
     );
