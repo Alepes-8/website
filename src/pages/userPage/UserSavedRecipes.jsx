e6ce48d3f29c22cbea225feb0868cb67e35e35c4
Alex P <Alex.peschel@gmail.com>
2022-12-21 18:02:56 +0100

    delete selfe made recipes

--- src/pages/userPage/UserSavedRecipes.jsx
+++ src/pages/userPage/UserSavedRecipes.jsx
@@ -2,35 +2,14 @@ import './userSavedRecipes.css';
 import React from 'react';
 import {RecipeFeatured} from '../../components';
 
-const RecipeData = [
-    {
-      title: "Rissoto",
-      tag: "A simple but wonderfull recipe for you friday date",
-      time: "30"
-    },
-    {
-        title: "Alaskan Hot wings",
-        tag: "Have you ever had a wing recipe with snow in it?",
-        time: "1h"
-      },
-      {
-        title: "Rissoto",
-        tag: "A simple but wonderfull recipe for you friday date",
-        time: "30"
-      },
-      {
-          title: "Alaskan Hot wings",
-          tag: "Have you ever had a wing recipe with snow in it?",
-          time: "1h"
-        },
-  ];
 
-const UserSavedRecipes = () => {
+
+const UserSavedRecipes = ({recipes}) => {
   
   return (
     <div>
         <div className='Saved_Recipes'>
-          {RecipeData.map((item, index) => <RecipeFeatured recipe={item} />)}
+          {recipes.map((item, index) => <RecipeFeatured recipe={item} />)}
         </div>
     </div>
   );
