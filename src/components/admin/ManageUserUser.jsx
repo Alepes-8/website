184e46aa8c4fb89c209de9fa84f5e831ff7602af
Alex P <Alex.peschel@gmail.com>
2022-12-28 23:14:23 +0100

    set base for manageUsers

--- src/components/admin/ManageUserUser.jsx
+++ src/components/admin/ManageUserUser.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
 import './manageUserUser.css';
 
 const ManageUsersUser = ({privilege, user}) => {
+    const [showing, setShowing] = useState("flex");
+    console.log(user);
     const ChangeAdminStatus = () => {
         alert("no ChangeAdminStatus functionality made yet");
     }
 
-   const DeleteUser = () => {
-    alert("no DeleteUser functionality made yet");
+    //TODO Test and add feature. Not testable with romans changes
+   const DeleteUser = async() => {
+    /*alert("no DeleteUser functionality made yet");
+
+    await fetch(`/users/${user.email}/`,{
+        method: 'DELETE',
+    }).then((response) => {
+        console.log(response)
+        if(response.status !== 204){
+            if(response.status === 500){
+                alert("Delete failed. Error on the server side. Error 500");
+            }else{
+                alert("The user was not deleted as it should")
+            }
+            return;
+        }
+    }).catch(function(error){
+        console.log('ERROR:', error)
+        return;
+    })*/
+    setShowing('none');
    }
     
     return( 
-        <div className='User_Template'>
+        <tr id="testing" style={{display: `${showing}`}}  className='User_Template'>
             
             <button onClick={DeleteUser}> delete</button>
             <div className='placement'>
@@ -21,14 +43,14 @@ const ManageUsersUser = ({privilege, user}) => {
             <b>UserName:</b> {user.email} 
             {privilege
                 ? <div><div className='toggle_Box'>
-                    <input type="checkbox" defaultChecked={user.admin} onClick={ChangeAdminStatus} />
+                    <input type="checkbox" defaultChecked={user.is_staff} onClick={ChangeAdminStatus} />
                     
                 </div>
                 <strong>{"Admin"}</strong>
                 </div>: <p></p>
             }
                
-        </div>
+        </tr>
     );
 }
       
