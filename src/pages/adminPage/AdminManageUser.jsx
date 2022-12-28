490100e3db57cca3fca98cc965948e2125f895c0
Alex P <Alex.peschel@gmail.com>
2022-12-28 23:05:15 +0100

    cleanUp

--- src/pages/adminPage/AdminManageUser.jsx
+++ src/pages/adminPage/AdminManageUser.jsx
@@ -4,18 +4,6 @@ import {ManageUserUser} from '../../components'
 
 
 
-const DataUser = [
-  {
-    username: "user1",
-    password: "pass1", 
-    admin: false,
-    supAdmin:false
-  },
-  
-];
-
-
-
 const AdminManageUser = ({ privilege, dataUsers}) => {
   return (
     <div>
