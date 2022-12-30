e4f1893a9d5a58a2fe39fcc3085f9d05597b1a5b
Alex P <Alex.peschel@gmail.com>
2022-12-30 17:28:08 +0100

    delete unasusary code and errors

--- src/pages/adminPage/AdminManageRecipes.jsx
+++ src/pages/adminPage/AdminManageRecipes.jsx
@@ -14,20 +14,13 @@ const AdminManageRecipes = ({recipes,slugs}) => {
     return (
         <div>
             {recipes.map((item) => 
-                    
-                    <ManageUserRecipe recipes ={recipes} recipe={item} 
-                    slugData={slugs.filter((element) => {
-                        if(element.recipe.id === item.id){
-                            return element;
-                        }
-                    })
-                    }index={index +=1}/
-                >
-                
-            
-               
+                <ManageUserRecipe 
+                    recipes ={recipes} 
+                    recipe={item} 
+                    slugData={slugs.filter((element) => element.recipe.id === item.id)}
+                    index={index +=1}
+                />
             )}
-            
         </div>
     )
 
