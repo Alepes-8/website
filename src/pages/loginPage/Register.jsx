45c315c0663649c9ac00a217b661d8146c551cf1
Alex P <Alex.peschel@gmail.com>
2023-01-03 16:55:46 +0100

    add register acount

--- src/pages/loginPage/Register.jsx
+++ src/pages/loginPage/Register.jsx
@@ -1,5 +1,22 @@
 import React, {useState} from 'react'
 import './authentication.css';
+import axios from 'axios';
+
+axios.defaults.headers.common = {Accept:`application/json`}
+
+
+axios.interceptors.request.use(
+  config => {
+      const token = "c861931e674bb10b1989374269af8fd661584218";
+      if (token) {
+          config.headers['Authorization'] =  `Token ${token}`;
+      }
+      config.headers['Content-Type'] = 'application/json';
+      return config;
+  },
+  error => {
+      Promise.reject(error)
+});
 
 const Register = ({setToken}) => {
     const [errorMessages, setErrorMessages] = useState("");
@@ -11,7 +28,7 @@ const Register = ({setToken}) => {
     const handleRegister = async() => {
       setErrorMessages("creating")
       //check the validation of an email input
-      let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+     /* let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if ( !re.test(regUserEmail) ) {
         setErrorMessages("email is bad")
         return;
@@ -20,10 +37,35 @@ const Register = ({setToken}) => {
       if(regPassword !== controlPass){
         setErrorMessages("password does not match")
         return;
-      }
+      }*/
       //alt 1 for the solution
       //Here we try to create it and if the server say no then either username or 
       //email is already in use. Then just give a not clear answer.
+
+      const user = {
+        "id": 1,
+        "is_superuser": false,
+        "is_staff": false,
+        "email": regUserEmail,
+        "password": "test",
+        "savedRecipes": [],
+        "createdRecipes": []
+      }
+      
+      axios.post('http://127.0.0.1:8000/users/', user).then(res => 
+      {
+        
+        console.log(res)
+        if(res.status ===404){
+          setErrorMessages("email already existing")
+        }else if(res.status === 500){
+          setErrorMessages("Try Again later, server seems to not respond")
+        }else if(res.status === 201){
+          setErrorMessages("Success, acount created")
+        }
+
+      })
+      /*
       fetch('/users/', {
         method:'POST',
         headers:{
@@ -64,7 +106,7 @@ const Register = ({setToken}) => {
         setErrorMessages(`${error}`);
         console.log('ERROR:', error)
         return;
-      })
+      })*/
           }
     
 
